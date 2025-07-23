a5b8388cc413216c2e5c4b75ad179bd8a25bfd01
Harshvardhan Shinde <136096571+H-Shinde@users.noreply.github.com>
2025-07-23 11:21:29 -0700

    Made a webApp with React tailwindcss and finished Login Page frontend with developing Create New Account and HomePage

---
+++ medicalSpanishApp/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home.jsx";
+import NewAccount from "./pages/NewAccount.jsx";
+
+export default function App() {
+  return (
+    <div className="w-full">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home/>} />
+          <Route path="/createAccount" element={<NewAccount/>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
